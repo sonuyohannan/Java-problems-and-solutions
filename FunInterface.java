a0c4900aa9617ee9e30d5b30340033f885c3649c
sonu yohannan <sonuyohannan@gmail.com>
2023-09-11 22:56:01 +0530

    lamda expression problem added

---
+++ FunInterface.java
@@ -0,0 +1,12 @@
+import java.lang.FunctionalInterface;
+    // functional interface 
+@FunctionalInterface
+interface FunInterface {
+    //abstract method
+    double grtPivalue();
+
+    
+    
+}
+
+
